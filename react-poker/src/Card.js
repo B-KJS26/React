9b0ddb65f0cccb9ffe2eab35fcec2f3083451ba0
B-KJS26 <digyrh456789@gmail.com>
2022-07-05 09:19:22 +0900

    z

--- react-poker/src/Card.js
+++ react-poker/src/Card.js
@@ -6,13 +6,18 @@ import './fourth.css';
 import './fifthcard.css'
 
 function Card() {
+    let counted = 0;
     let allcounting = 0;
     var firstcount = false;
+    let firstcounting = 0;
+    let secondcounting = 0;
+    let thirdcounting = 0;
+    let fourthcounting = 0;
+    let fifthcounting = 0;
     function show() {
         firstcount = !firstcount;
         if(firstcount) allcounting++;
         else allcounting--;
-
         if(firstcount){
         document.getElementById("holdone").style.display = 'block';}
         else
@@ -56,24 +61,89 @@ function Card() {
         else
         document.getElementById("holdfive").style.display = 'none';
     }
+
     function shuffling() {
-        if(allcounting === 1 || allcounting === 0){
-            alert('No pair');
+        if(!firstcount){
+            if(allcounting%2 === 0){
+                firstcounting = 2;
+            document.getElementById('first').className = "changed";
+            }
+            else if(allcounting%2 !== 0){
+                firstcounting = 7;    
+            document.getElementById('first').className = "joker";
+            }
         }
-        else if(allcounting === 2){
-            alert('Two Pair');
+        if(!secount){
+            if(allcounting%2 === 0){
+                secondcounting = 5;
+            document.getElementById('second').className = "changedd";
+            }
+            else if(allcounting%2 !== 0){   
+                secondcounting = 8; 
+            document.getElementById('second').className = "eclov";
+            }
         }
-        else if(allcounting === 3){
-            alert('Three of a kind');
+        if(!thcount){
+            if(allcounting%2 === 0){
+                thirdcounting = 10;
+                document.getElementById('third').className = "theart";
+            }
+                else if(allcounting%2 !== 0){    
+                thirdcounting = 7;
+                document.getElementById('third').className = "sdia";
+            }
         }
-        else
-        alert('Four Of A Kind');
+        if(!focount){
+            if(allcounting%2 === 0){
+                fourthcounting = 6;
+                document.getElementById('fourth').className = "fspade";
+            }
+                else if(allcounting%2 !== 0){    
+                fourthcounting = 4;
+                document.getElementById('fourth').className = "sixclov";
+            }
+        }
+        if(!ficount){
+            if(allcounting%2 === 0){
+                fifthcounting = 4;
+                document.getElementById('fifth').className = "fclov";
+            }
+                else if(allcounting%2 !== 0){ 
+                fifthcounting = 4;   
+                document.getElementById('fifth').className = "fdia";
+            }
+        }
+        if(firstcounting === secondcounting || firstcounting === thirdcounting || firstcounting === fourthcounting || firstcounting === fifthcounting){
+            alert('Two pair');
+        }
+        if(secondcounting === thirdcounting || secondcounting === fourthcounting || secondcounting === thirdcounting){
+            alert('Two pair');
+        }
+        if(thirdcounting === fourthcounting || thirdcounting === fifthcounting){
+            alert('Two pair');
+        }
+        if(fourthcounting === fifthcounting){
+            alert('Two pair');
+        }
+        if((firstcounting === secondcounting && firstcounting === thirdcounting) || (firstcounting === secondcounting && firstcounting === fourthcounting) || (firstcounting === secondcounting && firstcounting === fifthcounting)){
+            alert('Three of a Kind');
+        }
+        if((firstcounting === thirdcounting && firstcounting === fourthcounting) || (firstcounting === thirdcounting && firstcounting === fifthcounting)){
+            alert('Three of a Kind');
+        }
+        if((firstcounting === fourthcounting && firstcounting === fifthcounting)){
+            alert('Three of a Kind');
+        }
+        if(counted === 0){
+        alert('Four of a Kind');
+        counted++;
+        }
     }
     return(
             <div className="wrapper">
                 <div className='content'>
                     <div className ="scale">
-                        <div className = "imgesa" id = "first" onClick={show}>
+                        <div class = "imgesaf" id = "first" onClick={show}>
                             <h1 id="holdone">Hold</h1>
                         </div>
                     </div>
@@ -81,7 +151,7 @@ function Card() {
                 <br></br>
                 <div className='content'>
                     <div className ="scale">
-                        <div className = "imgesa" id = "second" onClick={show2}>
+                        <div class = "imgesas" id = "second" onClick={show2}>
                             <h1 id="holdsec">Hold</h1>
                         </div>
                     </div>
@@ -89,7 +159,7 @@ function Card() {
                 <br></br>
                 <div className='content'>
                     <div className ="scale">
-                        <div className = "imgesa" id = "third" onClick={show3}>
+                        <div class = "imgesath" id = "third" onClick={show3}>
                             <h1 id="holdth">Hold</h1>
                         </div>
                     </div>
@@ -97,7 +167,7 @@ function Card() {
                 <br></br>
                 <div className='content'>
                     <div className ="scale">
-                        <div className = "imgesa" id = "fourth" onClick={show4}>
+                        <div class = "imgesaf" id = "fourth" onClick={show4}>
                             <h1 id="holdfour">Hold</h1>
                         </div>
                     </div>
@@ -105,7 +175,7 @@ function Card() {
                 <br></br>
                 <div className='content'>
                     <div className ="scale">
-                        <div className = "imgesa" id = "fifth" onClick={show5}>
+                        <div class = "imgesafi" id = "fifth" onClick={show5}>
                             <h1 id="holdfive">Hold</h1>
                         </div>
                     </div>
